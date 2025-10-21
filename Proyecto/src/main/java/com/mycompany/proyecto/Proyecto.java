81ee66f0b3e108e0a2671549301e88802b888677
vmsanchez4 <vmsanchez4@espe.edu.ec>
2025-10-21 17:59:14 -0500

    Se agrega el proyecto

---
+++ Proyecto/src/main/java/com/mycompany/proyecto/Proyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto;
+
+/**
+ *
+ * @author MEGAPC
+ */
+public class Proyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
