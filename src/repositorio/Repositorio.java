8cec8118958fb8bb3a59dc2c1a66c5662c052967
SantiagoJC16 <javierchafla90@gmail.com>
2025-10-21 19:06:40 -0500

    Hola vicente

---
+++ src/repositorio/Repositorio.java
@@ -0,0 +1,18 @@
+
+package repositorio;
+
+/**
+ *
+ * @author javie
+ */
+public class Repositorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Vicentenario");
+    }
+    
+}
