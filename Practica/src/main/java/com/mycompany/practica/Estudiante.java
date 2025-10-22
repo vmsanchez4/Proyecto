f9315467866e02c998b09a77c6d89f940d717843
vmsanchez4 <vmsanchez4@espe.edu.ec>
2025-10-21 21:00:45 -0500

    Clase estudiante

---
+++ Practica/src/main/java/com/mycompany/practica/Estudiante.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practica;
+
+/**
+ *
+ * @author MEGAPC
+ */
+public class Estudiante {
+    
+    String nombre;
+    String cedula;
+    int edad;
+    
+    public Estudiante(){
+    
+    }
+    
+    public void mostrarInformacion(){
+        System.out.println("Su nombre es: " + nombre);
+    }
+}
